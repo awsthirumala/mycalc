a21807578f8a6ed01ba597072d5e0e8783cf2dff
tom <tom@example.com>
2018-04-26 11:01:54 +0000

    first commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+  @Test
+  public void evaluatesExpression() {
+    Calculator calculator = new Calculator();
+    int sum = calculator.evaluate("1+2+3");
+    assertEquals(6, sum);
+  }
+
+  @Test
+  public void evaluatesExpressionFalse() {
+    Calculator calculator = new Calculator();
+    int sum = calculator.evaluate("4+2+3");
+    assertEquals(8, sum);
+  }
+
+}
+
